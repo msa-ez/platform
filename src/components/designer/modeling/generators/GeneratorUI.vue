c93006807f3ff8060061746a930b63fa7569b8cf
kibumpark <top5w5@naver.com>
2023-12-28 14:36:02 +0900

    오토모델링 버튼 사라짐 수정

--- src/components/designer/modeling/generators/GeneratorUI.vue
+++ src/components/designer/modeling/generators/GeneratorUI.vue
@@ -66,7 +66,7 @@
                     </v-tooltip> -->
                     <v-tooltip bottom>
                         <template v-slot:activator="{ on, attrs }">
-                            <v-btn v-if="generatorStep === 'aggregate'"
+                            <v-btn v-if="generatorStep === 'aggregate' || generatorName === 'CJMGenerator' || generatorName === 'BMGenerator'"
                                 @click="finishModelCreation()"
                                 small
                                 v-bind="attrs"
@@ -75,8 +75,14 @@
                                 style="padding:0px 5px; margin-right:10px;"
                                 color="primary"
                             >
-                                <span style="float:right;">CONTINUE</span>
-                                <v-icon>mdi-arrow-right</v-icon>
+                                <div v-if="generatorName === 'CJMGenerator' || generatorName === 'BMGenerator'">
+                                    <Icon style="float:left; margin-right:3px;" icon="fluent-mdl2:completed" width="16" height="16"/>
+                                    <span>complete</span>
+                                </div>
+                                <div v-else>
+                                    <span style="float:right;">CONTINUE</span>
+                                    <v-icon>mdi-arrow-right</v-icon>
+                                </div>
                             </v-btn>
                         </template>
                         <span>Auto modeling completed</span>
