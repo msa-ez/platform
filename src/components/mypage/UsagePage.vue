16b59993750c29e3dfa807ead4e115662d527043
sooheon45 <sooheon45@uengine.org>
2024-02-01 16:21:31 +0900

    다운로드 csv 처리.

--- src/components/mypage/UsagePage.vue
+++ src/components/mypage/UsagePage.vue
@@ -218,6 +218,35 @@ export default {
     
                 return type
             },
+            downloadCsv(csvData, title){
+                const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+                const url = URL.createObjectURL(blob);
+
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${title}.csv`);
+                document.body.appendChild(link);
+
+                link.click();
+
+                document.body.removeChild(link);
+                URL.revokeObjectURL(url);
+            },
+            stringifyObjectValues(obj) {
+                const result = {};
+                for (const key in obj) {
+                    const value = obj[key];
+
+                    if (typeof value === 'object' && value !== null) {
+                        result[key] = JSON.stringify(value);
+                    } else {
+                        result[key] = value;
+                    }
+                    
+                }
+
+                return result;
+            }
         }
     }
 </script>
