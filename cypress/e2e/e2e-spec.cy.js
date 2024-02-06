211e6e031101fb9b6129508404adc79941e9aa72
kyusoo <rbtn110@uengine.org>
2024-02-06 09:29:04 +0900

    cypress ignore

--- cypress/e2e/e2e-spec.cy.js
+++ cypress/e2e/e2e-spec.cy.js
@@ -1,7 +1,7 @@
 describe('Auto Modeling Test', () => {
   
   it('visit page and log in.', () => {
-    cy.login('rbtn110@uengine.org', 'Rlarbtn135!')
+    cy.login('')
     
     cy.visit('https://www.msaez.io:8081/#/')
     
@@ -11,6 +11,7 @@ describe('Auto Modeling Test', () => {
     
 
     const yourText = '음식 배달 앱';
+    
     cy.get('.auto-modeling-text').type(`${yourText}{enter}`);
 
     cy.get('.auto-cjm').click()
