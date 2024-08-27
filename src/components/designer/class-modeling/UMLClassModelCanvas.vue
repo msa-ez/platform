c2db82b685938f70fe386f3ebae8818b23b3ae20
dhtns818 <dhtns818@rosesystems.kr>
2024-08-27 17:14:46 +0900

    fix uml canvas relation edit issue

--- src/components/designer/class-modeling/UMLClassModelCanvas.vue
+++ src/components/designer/class-modeling/UMLClassModelCanvas.vue
@@ -357,8 +357,7 @@
                                     append-icon="mdi-send"
                                     @click:append="generate()"
                                     @keypress.enter="debouncedGenerate()"
-                                >
-                                </v-text-field>                                     
+                                ></v-text-field>                                     
                             </v-card>
                         </v-card>
                     </v-layout>
@@ -778,7 +777,12 @@
                         me.$set(aggEl.aggregateRoot, "fieldDescriptors", rootClass.fieldDescriptors);
                         me.$set(aggEl.aggregateRoot, "operations", rootClass.operations);
                     }
-                    me.$set(aggEl.aggregateRoot, "entities", JSON.parse(JSON.stringify(me.value)));
+                    if (diff.elements) {
+                        me.$set(aggEl.aggregateRoot.entities, "elements", JSON.parse(JSON.stringify(me.value.elements)));
+                    }
+                    if (diff.relations) {
+                        me.$set(aggEl.aggregateRoot.entities, "relations", JSON.parse(JSON.stringify(me.value.relations)));
+                    }
                     parentCanvas.updateUMLClassValue(aggEl);
                 }
             }
