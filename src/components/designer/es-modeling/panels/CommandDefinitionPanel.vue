15cd3268d6b745a4ffd533b759f0c749ee571eef
kyusoo <rbtn110@uengine.org>
2024-10-11 09:53:15 +0900

    extend verb PATCH 제거

--- src/components/designer/es-modeling/panels/CommandDefinitionPanel.vue
+++ src/components/designer/es-modeling/panels/CommandDefinitionPanel.vue
@@ -219,7 +219,7 @@
                 return this.restfulList.slice(1)
             },
             getControllerList(){
-                return this.restfulList.filter(method => method === 'POST' || method === 'PUT' || method === 'DELETE' || method === 'PATCH');
+                return this.restfulList.filter(method => method === 'POST' || method === 'PUT' || method === 'DELETE');
             },
             elementPrefix(){
                 var me = this
@@ -416,8 +416,6 @@
                             me.commandExample = `http PUT localhost:8080/${pluralize(changeCase.camelCase(me.relatedAggregateName))}/1/${me.value.controllerInfo.apiPath} ${descriptorName}="{value}"`
                         }else if(me.value.controllerInfo.method == 'DELETE'){
                             me.commandExample = `http DELETE localhost:8080/${pluralize(changeCase.camelCase(me.relatedAggregateName))}/1/${me.value.controllerInfo.apiPath} ${descriptorName}="{value}"`
-                        }else if(me.value.controllerInfo.method == 'PATCH'){
-                            me.commandExample = `http PATCH localhost:8080/${pluralize(changeCase.camelCase(me.relatedAggregateName))}/1/${me.value.controllerInfo.apiPath} ${descriptorName}="{value}"`
                         }
                         me.setApiPath()
                     }
