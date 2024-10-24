72e7e94ad01753f76530c411f59dd397d730ac91
jaehyeong <jaeh@uengine.org>
2024-10-24 12:02:52 +0900

    closed #61 VO, enum class example & fix error si gpt

---
+++ src/components/primitives/Enum.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-select
+            v-bind="$attrs"
+            v-model="value"
+            @change="change"
+            :items="items"
+            item-text="label"
+            item-value="value"
+            :label="label"
+        >
+        </v-select>
+        <v-chip 
+            class="gs-rule-example-chip"
+            v-for="chip in chips"
+            :key="chip.label"
+            @click="selectChipValue(chip.value)"
+        >
+            {{ chip.label }}
+        </v-chip>
+    </div>
+</template>
+<script>
+import TypeBase from './TypeBase.js';
+
+export default {
+    name: 'String',
+    mixins: [TypeBase],
+    props: {
+        items: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
