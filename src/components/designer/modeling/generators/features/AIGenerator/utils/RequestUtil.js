4cef8d04760cb0fa81f69bd921790de907aae1c7
ShinSeongJin2 <ShinSeongJin@uengine.org>
2025-02-25 17:43:58 +0900

    Fix: OpenAI API 요청 관련 네트워크 문제 해결 작업업

--- src/components/designer/modeling/generators/features/AIGenerator/utils/RequestUtil.js
+++ src/components/designer/modeling/generators/features/AIGenerator/utils/RequestUtil.js
@@ -35,7 +35,6 @@ class RequestUtil {
   static sendPostRequest(url, data, headers = {}, onProgress, onLoadEnd, onError, resolve, reject) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
-    xhr.withCredentials = true;
 
     Object.keys(headers).forEach(header => {
       xhr.setRequestHeader(header, headers[header]);
